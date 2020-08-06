9a37644ea7830e9b2133d66ce6bd0efe8d903de3
shinharad <shin.harad@gmail.com>
2020-08-07 08:26:43 +0900

    config

--- config/src/main/scala/com.example.config/part2/07_Misc.scala
+++ config/src/main/scala/com.example.config/part2/07_Misc.scala
@@ -19,11 +19,12 @@ object `07_Misc` extends App {
   import Config._
 
   final case class Config(
-      config: AlosoConfig,
+      config: AlosoConfig, // 実行パラメータより
       db: Db,
       redis: Redis,
       length: Length,
-      temperature: Temperature
+      temperature: Temperature,
+      KEY1: String // 環境変数より
     ) extends NestedRendering(level = 1)
 
   object Config {
@@ -56,7 +57,7 @@ object `07_Misc` extends App {
       import pureconfig.generic._
       import pureconfig.generic.auto._
       import pureconfig.module.squants._
-      
+
       implicit def productHint[A]: ProductHint[A] =
         ProductHint(ConfigFieldMapping(CamelCase, CamelCase))
 
@@ -68,6 +69,78 @@ object `07_Misc` extends App {
   }
 
   println(Config.instance)
+
+  println("-" * 50)
+}
+
+// bloop run config -m com.example.config.part2.07_Misc2 -- -J-Dconfig.override_with_env_vars="true"
+object `07_Misc2` extends App {
+  println("-" * 50)
+
+  import Config._
+
+  final case class Config(
+      config: AlosoConfig,
+      db: Db,
+      redis: Redis,
+      length: Length,
+      temperature: Temperature,
+      KEY1: String
+    ) extends NestedRendering(level = 1)
+
+  object Config {
+    final case class AlosoConfig(
+        override_with_env_vars: Boolean Refined Equal[true]
+      ) extends NestedRendering(level = 2) {
+      override val productPrefix: String =
+        "Config"
+    }
+
+    final case class Db(
+        uri: URI,
+        user: NonEmptyString,
+        pw: NonEmptyString,
+        timeout: Duration
+      ) extends NestedRendering(level = 2)
+
+    final case class Redis(
+        host: URI,
+        port: UserPortNumber,
+        timeout: Duration
+      ) extends NestedRendering(level = 2)
+
+    val instance: (Config, com.typesafe.config.ConfigValue) = {
+      import com.typesafe._
+
+      import eu.timepit.refined.pureconfig._
+
+      import pureconfig._
+      import pureconfig.generic._
+      import pureconfig.generic.auto._
+      import pureconfig.module.squants._
+
+      implicit def productHint[A]: ProductHint[A] =
+        ProductHint(ConfigFieldMapping(CamelCase, CamelCase))
+
+      val read = ConfigSource
+        .fromConfig(config.ConfigFactory.load("part2/application4.conf"))
+        .loadOrThrow[Config]
+
+      val write =
+        ConfigWriter[Config]
+          .to(read)
+
+      read -> write
+
+    }
+  }
+
+  val (read, write) = Config.instance
+
+  println(read)
+  println("-" * 50)
+
+  println(write.render)
 
   println("-" * 50)
 }
